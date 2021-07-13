0a4fea87a5cf8ea0b418183deb977316c33ad10a
ThatGuyJop <45967788+ThatGuyJop@users.noreply.github.com>
2021-07-13 22:49:27 +0200

    Update config.lua

--- SocietyLogs/config.lua
+++ SocietyLogs/config.lua
@@ -14,19 +14,16 @@
 
 Autodealer = {}
 
-Autodealer.WHCFG{
-    Color = "#cc66ff",
-    Inline = true,
-}
+Autodealer.Color = "#cc66ff"
 
 -- Autodealer.SocietyNaam = "",
 -- Autodealer.SocietyRekening = "",-- Society rekening hier (society-autodealer)
-Autodealer.GuildID = "DISCORD_WEBHOOK",-- Guild ID hier
-Autodealer.VerkoopWH = "DISCORD_WEBHOOK",-- Verkoop Webhook ID hier
-Autodealer.InkoopWH = "DISCORD_WEBHOOK",-- Inkoop Webhook ID hier
-Autodealer.GeldWH = "DISCORD_WEBHOOK",-- Geld Webhook ID hier
-Autodealer.KluisWH = "DISCORD_WEBHOOK",-- Kluis Webhook ID hier
-Autodealer.StockWH = "DISCORD_WEBHOOK",-- Stock Webhook ID hier
+Autodealer.GuildID = "DISCORD_WEBHOOK"-- Guild ID hier
+Autodealer.VerkoopWH = "DISCORD_WEBHOOK"-- Verkoop Webhook ID hier
+Autodealer.InkoopWH = "DISCORD_WEBHOOK"-- Inkoop Webhook ID hier
+Autodealer.GeldWH = "DISCORD_WEBHOOK"-- Geld Webhook ID hier
+Autodealer.KluisWH = "DISCORD_WEBHOOK"-- Kluis Webhook ID hier
+Autodealer.StockWH = "DISCORD_WEBHOOK"-- Stock Webhook ID hier
 
 AutoDealer.steamID = true				-- set to false to disable Steam ID in the logs
 AutoDealer.steamURL = true				-- set to false to disable Steam URL in the logs
